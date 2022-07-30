95c67d8240ba9d52e427824cf1a3c37d074398e5
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-30 02:43:18 -0300

    Agrego data para pruebas

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const userList = [
+  "Pedro Gonzalez",
+  "Juana Perez",
+  "kala Cordara",
+  "Jose Pepe",
+  "Gustavo Adn",
+  "Jeremias Sissino",
+  "Luis Alberto",
+];
+
+export const fileList = [
+  {
+    name: "Factura",
+    createDate: new Date(),
+    documentType: "Privado",
+    owner: "Ruben",
+    link: "https://drive.google.com/uc?id=1fvUjURyjpCysp3wcA9Oadji88oNsYoE-&export=download",
+  },
+  {
+    name: "Factura",
+    createDate: new Date(),
+    documentType: "Privado",
+    owner: "Ruben",
+    link: "https://drive.google.com/uc?id=1fvUjURyjpCysp3wcA9Oadji88oNsYoE-&export=download",
+  },
+  {
+    name: "Factura",
+    createDate: new Date(),
+    documentType: "Privado",
+    owner: "Ruben",
+    link: "https://drive.google.com/uc?id=1fvUjURyjpCysp3wcA9Oadji88oNsYoE-&export=download",
+  },
+  {
+    name: "Factura",
+    createDate: new Date(),
+    documentType: "Privado",
+    owner: "Ruben",
+    link: "https://drive.google.com/uc?id=1fvUjURyjpCysp3wcA9Oadji88oNsYoE-&export=download",
+  },
+];
