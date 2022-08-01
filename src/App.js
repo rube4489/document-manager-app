f8532023dfb4f2af97d7d7bc128168cbabda5334
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-31 23:57:19 -0300

    Hago get al store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import TableScreen from "./components/TableScreen";
+import { getItems } from "./api/api";
 
-function App() {
+const App = () => {
+  const [userListAux, setUserListAux] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const data = await getItems();
+    setUserListAux(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TableScreen userListAux={userListAux} setUserListAux={setUserListAux} />
   );
-}
+};
 
 export default App;
