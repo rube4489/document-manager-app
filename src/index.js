4d2b4deb51a28c28022692aa988d1b3b0b368994
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-31 23:57:53 -0300

    Import App

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import TableScreen from "./components/TableScreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TableScreen />);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
